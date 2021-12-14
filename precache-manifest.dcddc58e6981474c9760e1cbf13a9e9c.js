30c6ff89d76998d7739e69b65d975d8d5c18b744
Denis Iliescu <denisiliescu@gmail.com>
2021-12-14 19:56:45 +0200

    Updates

---
+++ precache-manifest.dcddc58e6981474c9760e1cbf13a9e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e121150df4882841016942292fae3465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c615a3e13a1c507fde00",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "8e47bf352e888ac5d7b5",
+    "url": "/static/js/2.e508c0e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.e508c0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c615a3e13a1c507fde00",
+    "url": "/static/js/main.a7d30348.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e70eb5425be285b5cdf30f35b849e022",
+    "url": "/static/media/profilepic.e70eb542.jpg"
+  },
+  {
+    "revision": "9333703ced28a43565e9e4e0dc8beb15",
+    "url": "/static/media/resume.9333703c.pdf"
+  }
+]);
